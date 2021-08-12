698b4fa45599345bfbfd17c5a0bc36713f7e4490
tofigamraslanov <tofiq.amraslanov.t@asoiu.edu.az>
2021-08-12 14:04:44 +0400

    NewExpense component added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
 const App = () => {
@@ -25,6 +26,11 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
   // Behind The Scenes
   // return React.createElement(
   //   'div',
@@ -35,7 +41,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );

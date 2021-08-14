772e1b513825567f84a26a1d5204adf5f1ca759b
tofigamraslanov <tofiq.amraslanov.t@asoiu.edu.az>
2021-08-15 01:07:51 +0400

    Improved

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = props => {
+  const [isClicked, setIsClicked] = useState(false);
+
   const saveExpenseDataHandler = enteredExpenseData => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsClicked(false);
   };
 
+  const clickHandler = () => {
+    setIsClicked(true);
+  };
+
+  const cancelHandler = () => {
+    setIsClicked(false);
+  };
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isClicked && <button onClick={clickHandler}>Add Expense</button>}
+      {isClicked && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={cancelHandler}
+        />
+      )}
     </div>
   );
 };

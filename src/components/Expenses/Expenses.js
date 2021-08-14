c5e7a04df8aed90ccc6cd3bef1c070a08f4db3fa
tofigamraslanov <tofiq.amraslanov.t@asoiu.edu.az>
2021-08-15 00:19:09 +0400

    Showing different results based on condition done

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -25,17 +25,6 @@ const Expenses = props => {
           selected={filteredYear}
         />
         <ExpensesList items={filteredExpenses} />
-
-        {/* {filteredExpenses.length === 0 && <p>No expenses found.</p>}
-        {filteredExpenses.length > 0 &&
-          filteredExpenses.map(expense => (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          ))} */}
       </Card>
     </div>
   );
